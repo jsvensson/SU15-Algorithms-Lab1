ab65322296bc023fd7e9f31ecfd0a2c58085da33
Johan Svensson <johan@atomicplayboy.net>
2016-09-03 10:22:14 +0200

    Check execution time
    Checkpoint

--- SU15-Alg-Lab1-Bonus/Problem5.cs
+++ SU15-Alg-Lab1-Bonus/Problem5.cs
@@ -12,11 +12,43 @@ namespace SU15_Alg_Lab1_Bonus
     {
         public static IEnumerable<IEnumerable<int>> FindPermutations()
         {
-            var digits = new Set<int> {1, 2, 3, 4};
+            var digits = new Set<int> {1, 2, 3, 4, 5, 6, 7, 8, 9};
             var set = new Set<Set<int>> {digits};
 
             Set<Set<int>> join1 = PermutateJoin(set);
             Set<Set<int>> join2 = PermutateJoin(join1);
+            Set<Set<int>> join3 = PermutateJoin(join2);
+            //Set<Set<int>> join4 = PermutateJoin(join3);
+            //Set<Set<int>> join5 = PermutateJoin(join4);
+            //Set<Set<int>> join6 = PermutateJoin(join5);
+            //Set<Set<int>> join7 = PermutateJoin(join6);
+            //Set<Set<int>> join8 = PermutateJoin(join7);
+
+            set.Add(join1);
+            set.Add(join2);
+            set.Add(join3);
+            //set.Add(join4);
+            //set.Add(join5);
+            //set.Add(join6);
+            //set.Add(join7);
+            //set.Add(join8);
+
+            Set<Set<int>> sub1 = PermutateSubtraction(set);
+            Set<Set<int>> sub2 = PermutateSubtraction(sub1);
+            Set<Set<int>> sub3 = PermutateSubtraction(sub2);
+            Set<Set<int>> sub4 = PermutateSubtraction(sub3);
+            Set<Set<int>> sub5 = PermutateSubtraction(sub4);
+            //Set<Set<int>> sub6 = PermutateSubtraction(sub5);
+            //Set<Set<int>> sub7 = PermutateSubtraction(sub6);
+            //Set<Set<int>> sub8 = PermutateSubtraction(sub7);
+            set.Add(sub1);
+            set.Add(sub2);
+            set.Add(sub3);
+            set.Add(sub4);
+            set.Add(sub5);
+            //set.Add(sub6);
+            //set.Add(sub7);
+            //set.Add(sub8);
 
             return set;
         }
@@ -46,15 +78,7 @@ namespace SU15_Alg_Lab1_Bonus
 
 
                     // Check if we've already generated this permutation
-                    bool exists = false;
-                    foreach (Set<int> e in permutations)
-                    {
-                        if (perm.EqualTo(e))
-                        {
-                            exists = true;
-                            break;
-                        }
-                    }
+                    bool exists = permutations.Any(e => perm.EqualTo(e));
 
                     if (!exists)
                     {
@@ -83,15 +107,7 @@ namespace SU15_Alg_Lab1_Bonus
 
 
                     // Check if we've already generated this permutation
-                    bool exists = false;
-                    foreach (Set<int> e in permutations)
-                    {
-                        if (perm.EqualTo(e))
-                        {
-                            exists = true;
-                            break;
-                        }
-                    }
+                    bool exists = permutations.Any(e => perm.EqualTo(e));
 
                     if (!exists)
                     {
--- SU15-Alg-Lab1-Bonus/Program.cs
+++ SU15-Alg-Lab1-Bonus/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,23 +14,37 @@ namespace SU15_Alg_Lab1_Bonus
             //ulong[] fib = Problem.FibonacciForLoop(100);
             //Console.WriteLine(string.Join(",", fib));
 
-
+            var startTime = DateTime.Now;
             IEnumerable<IEnumerable<int>> set = Problem5.FindPermutations();
+            var endTime = DateTime.Now;
+
+
+            Console.WriteLine($"Run time: {endTime - startTime}");
             Console.WriteLine($"{set.Count()} permutations");
             Console.WriteLine();
 
-            foreach (IEnumerable<int> permutation in set)
+
+            using (StreamWriter writer =
+                new StreamWriter("out.txt"))
             {
-                Console.WriteLine($"{{ {string.Join(", ", permutation)} }}");
-
-
-                //if (permutation.Sum() == 100)
-                //{
-                //    Console.WriteLine($"{{ {string.Join(", ", permutation)} }}");
-                //}
-
+                foreach (IEnumerable<int> permutation in set)
+                {
+                    writer.WriteLine($"{{ {string.Join(", ", permutation)} }}");
+                }
             }
 
+
+            //foreach (IEnumerable<int> permutation in set)
+            //{
+            //    Console.WriteLine($"{{ {string.Join(", ", permutation)} }}");
+            //    //if (permutation.Sum() == 100)
+            //    //{
+            //    //    Console.WriteLine($"{{ {string.Join(", ", permutation)} }}");
+            //    //}
+
+            //}
+
+            Console.ReadKey();
         }
 
         static void PrintSet<T>(IEnumerable<T> set)
