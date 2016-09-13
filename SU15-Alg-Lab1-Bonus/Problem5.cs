f14058fb6415c48884ce32a775a4a95a8f81d27e
Johan Svensson <johan@atomicplayboy.net>
2016-09-13 09:54:07 +0200

    Optimization pass

--- SU15-Alg-Lab1-Bonus/Problem5.cs
+++ SU15-Alg-Lab1-Bonus/Problem5.cs
@@ -13,27 +13,27 @@ namespace SU15_Alg_Lab1_Bonus
         private static int iterations;
         public static IEnumerable<IEnumerable<int>> FindPermutations()
         {
-            var digits = new Set<int> {1, 2, 3, 4, 5, 6, 7, 8, 9};
-            var set = new Set<Set<int>> {digits};
+            var digits = new Sequence {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            var set = new Set<Sequence> {digits};
 
             // Slow-ass join loop
-            //Set<Set<int>> join;
-            //for (int i = 0; i < digits.Count - 1; i++)
+            //Set<Sequence> join;
+            //int length = digits.Count - 1;
+            //for (int i = 0; i < length; i++)
             //{
-            //    var newSet = (Set<Set<int>>)set.Copy();
+            //    var newSet = (Set<Sequence>)set.Copy();
             //    join = PermutateJoin(newSet);
             //    set.Add(join);
             //}
 
-
-            Set<Set<int>> join1 = PermutateJoin(set);
-            Set<Set<int>> join2 = PermutateJoin(join1);
-            Set<Set<int>> join3 = PermutateJoin(join2);
-            Set<Set<int>> join4 = PermutateJoin(join3);
-            Set<Set<int>> join5 = PermutateJoin(join4);
-            //Set<Set<int>> join6 = PermutateJoin(join5);
-            //Set<Set<int>> join7 = PermutateJoin(join6);
-            //Set<Set<int>> join8 = PermutateJoin(join7);
+            Set<Sequence> join1 = PermutateJoin(set);
+            Set<Sequence> join2 = PermutateJoin(join1);
+            Set<Sequence> join3 = PermutateJoin(join2);
+            Set<Sequence> join4 = PermutateJoin(join3);
+            Set<Sequence> join5 = PermutateJoin(join4);
+            //Set<Sequence> join6 = PermutateJoin(join5);
+            //Set<Sequence> join7 = PermutateJoin(join6);
+            //Set<Sequence> join8 = PermutateJoin(join7);
 
             set.Add(join1);
             set.Add(join2);
@@ -44,14 +44,14 @@ namespace SU15_Alg_Lab1_Bonus
             //set.Add(join7);
             //set.Add(join8);
 
-            Set<Set<int>> sub1 = PermutateSubtraction(set);
-            Set<Set<int>> sub2 = PermutateSubtraction(sub1);
-            Set<Set<int>> sub3 = PermutateSubtraction(sub2);
-            Set<Set<int>> sub4 = PermutateSubtraction(sub3);
-            Set<Set<int>> sub5 = PermutateSubtraction(sub4);
-            //Set<Set<int>> sub6 = PermutateSubtraction(sub5);
-            //Set<Set<int>> sub7 = PermutateSubtraction(sub6);
-            //Set<Set<int>> sub8 = PermutateSubtraction(sub7);
+            Set<Sequence> sub1 = PermutateSubtraction(set);
+            Set<Sequence> sub2 = PermutateSubtraction(sub1);
+            Set<Sequence> sub3 = PermutateSubtraction(sub2);
+            Set<Sequence> sub4 = PermutateSubtraction(sub3);
+            Set<Sequence> sub5 = PermutateSubtraction(sub4);
+            //Set<Sequence> sub6 = PermutateSubtraction(sub5);
+            //Set<Sequence> sub7 = PermutateSubtraction(sub6);
+            //Set<Sequence> sub8 = PermutateSubtraction(sub7);
             set.Add(sub1);
             set.Add(sub2);
             set.Add(sub3);
@@ -66,46 +66,44 @@ namespace SU15_Alg_Lab1_Bonus
             return set;
         }
 
-        public static Set<Set<int>> PermutateJoin(Set<Set<int>> set)
+        public static Set<Sequence> PermutateJoin(IEnumerable<Sequence> set)
         {
-            var permutations = new Set<Set<int>>();
+            var permutations = new Set<Sequence>();
 
-            foreach (Set<int> perm in set)
+            foreach (Sequence seq in set)
             {
-
-                int length = perm.Count - 2;
-                for (int i = 0; i <= length; i++)
+                int length = seq.Count - 1;
+                for (int i = 0; i < length; i++)
                 {
-                    var copy = perm.Copy().ToList();
+                    List<int> copy = seq.ToList();
                     int join = int.Parse(copy[i].ToString() + copy[i + 1]);
 
                     // Insert join at index i, delete i+1 to i+2 that were joined
                     copy.Insert(i, join);
                     copy.RemoveRange(i + 1, 2);
 
-                    var newPerm = new Set<int> {copy};
+                    var newPerm = new Sequence {copy};
                     iterations++;
                     permutations.Add(newPerm);
                 }
-
             }
 
             return permutations;
         }
 
-        public static Set<Set<int>> PermutateSubtraction(Set<Set<int>> set)
+        public static Set<Sequence> PermutateSubtraction(IEnumerable<Sequence> set)
         {
-            var permutations = new Set<Set<int>>();
+            var permutations = new Set<Sequence>();
 
-            foreach (Set<int> perm in set)
+            foreach (Sequence seq in set)
             {
-                int length = perm.Count();
+                int length = seq.Count();
                 for (int i = 1; i < length; i++)
                 {
-                    var copy = (Set<int>)perm.Copy();
+                    List<int> copy = seq.ToList();
                     copy[i] *= -1;
                     iterations++;
-                    permutations.Add(copy);
+                    permutations.Add(new Sequence {copy});
                 }
             }
 
---
+++ SU15-Alg-Lab1-Bonus/Sequence.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace SU15_Alg_Lab1_Bonus
+{
+    public class Sequence : IEnumerable<int>
+    {
+        private List<int> Numbers { get; set; }
+        public int Count => Numbers.Count;
+
+        public Sequence()
+        {
+            Numbers = new List<int>();
+        }
+
+        public void Add(int num)
+        {
+            Numbers.Add(num);
+        }
+
+        public int this[int index]
+        {
+            get { return Numbers[index]; }
+            set { Numbers[index] = value; }
+        }
+
+        public void Add(IEnumerable<int> nums)
+        {
+            Numbers.AddRange(nums);
+        }
+
+        public List<int> ToList()
+        {
+            return Numbers.ToList();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return Numbers.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", Numbers);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Sequence)
+            {
+                return ToString() == obj.ToString();
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+    }
+}
